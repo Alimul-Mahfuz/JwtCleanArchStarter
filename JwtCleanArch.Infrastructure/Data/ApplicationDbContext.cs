157e4d79a22de6db9a129b854222d51f557b4e16
Alimul-Mahfuz <alimulmahfuztushar@gamail.com>
2026-01-29 21:34:41 +0600

    Add project files.

---
+++ JwtCleanArch.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JwtCleanArch.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
